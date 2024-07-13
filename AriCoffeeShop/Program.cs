6a4aada30553e1629ac227878e17b23e4c9094d2
Arı Bilgi <aribilgiogretmen@gmail.com>
2024-07-13 13:35:53 +0300

    Last Commit - Areas

--- AriCoffeeShop/Program.cs
+++ AriCoffeeShop/Program.cs
@@ -31,4 +31,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
